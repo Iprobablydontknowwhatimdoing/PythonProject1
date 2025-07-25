e1301164c4b0bbb040c2a9fc6181dd7b1dd3fde7
iprobablydontknowwhatimdoing <octact82@gmail.com>
2025-07-24 21:00:42 -0700

    added braches

--- game.py
+++ game.py
@@ -19,7 +19,12 @@ def decide(decisions: List[str], show_by_default: Optional[bool]):
         print('Sorry, I didn\'t catch that, please try again')
         return decide(decisions, False)
 
-discoveredOptions: dict[str, bool] = {'guard change': False}
+discoveredOptions: dict[str, bool] = {'guard change': False, 'club': False, 'dagger': False, 'lock picks': False, 'key wax': False}
+
+class Graysoul:
+    def describe_outside_safehouse():
+        print("You are going where noone has gone before. This path has not been implemented yet.")
+        SmugglersHideout.describe_main_room(True)
 
 class SmugglersHideout:
     def describe_main_room(returning: Optional[bool]):
@@ -42,11 +47,11 @@ class SmugglersHideout:
             case 1:
                 SmugglersHideout.talk_to_smugglers()
             case 2:
-                SmugglersHideout.describe_back_hall(None)
+                SmugglersHideout.describe_back_hall(False)
             case 3:
                 Graysoul.describe_outside_safehouse()
     def talk_to_smugglers():
-        print("You are going where noone has gone before. This path has not been implimented yet.")
+        print("You are going where noone has gone before. This path has not been implemented yet.")
         SmugglersHideout.describe_main_room(True)
     def inspect_map():
         if discoveredOptions['guard change']:
@@ -71,34 +76,156 @@ class SmugglersHideout:
         decision = decide(["go to weapons", 'go to storage', 'go to bunks', 'go to main room'],True)
         match decision:
             case 0:
-                SmugglersHideout.describe_weapons_room()
+                SmugglersHideout.describe_weapons_room(discoveredOptions['dagger'] or discoveredOptions['club'])
             case 1:
                 SmugglersHideout.describe_storage_room()
             case 2:
                 SmugglersHideout.describe_bunk_room()
             case 3:
                 SmugglersHideout.describe_main_room(True)
-    def describe_weapons_room():
-        print("A small storage closet here is crammed full of useful tools, but on the top shelf of the closet, a few weapons gleam.")
+    def describe_weapons_room(returning: Optional[bool]):
+        if returning:
+            print("You are back in the closet")
+        else:
+            print("A small storage closet here is crammed full of useful tools, but on the top shelf of the closet, a few weapons gleam.\n"
+                  "Another shelf, lower down, holds a few tools, a couple of which you know how to use")
         decision = decide(['inspect weapons', 'inspect tools', 'return to hall'], False)
         match decision:
             case 0:
                 SmugglersHideout.inspect_weapons_in_storage()
             case 1:
-                SmugglersHideout.
-
+                SmugglersHideout.inspect_tools_in_storage()
+            case 2:
+                SmugglersHideout.describe_back_hall(True)
     def inspect_weapons_in_storage():
-        print("You are going where noone has gone before. This path has not been implimented yet.")
-        SmugglersHideout.describe_back_hall(True)
+        print(f"\nDEBUG: {discoveredOptions}\n")
+        if discoveredOptions['club']:
+            print('While the club fits nicely in your hand, you could switch it out with the dagger if you wanted, or drop the \n'
+                  'club back here. After all, you\'ve never needed a weapon before.')
+            decision = decide(['switch with dagger','leave club','go back'],False)
+            match decision:
+                case 0:
+                    print("The dagger falls into your hand heavily, though you are reluctant to leave the club here, you \n"
+                          "know you'll need at least one hand.")
+                    discoveredOptions['dagger'] = True
+                    discoveredOptions['club'] = False
+                    SmugglersHideout.describe_weapons_room(True)
+                case 1:
+                    print("You've never needed a weapon before, so why should you need one now? You leave the club on the shelf.")
+                    discoveredOptions['club'] = False
+                    SmugglersHideout.describe_weapons_room(True)
+                case 2:
+                    SmugglersHideout.describe_weapons_room(True)
+        elif discoveredOptions['dagger']:
+            print('While you already have a dagger, you know how to use the club better, and it can prove more useful in \n'
+                  'covert operations. Maybe it would be a better pick for you. You could switch the dagger out for the club,\n'
+                  'or you could leave the club back here. After all, you\'ve never needed a weapon before.')
+            decision = decide(['switch with club', 'leave dagger', 'go back'], False)
+            match decision:
+                case 0:
+                    print(
+                        "The club falls into your hand heavily, though you are reluctant to leave the dagger here, you \n"
+                        "know you'll need at least one hand.")
+                    discoveredOptions['dagger'] = False
+                    discoveredOptions['club'] = True
+                    SmugglersHideout.describe_weapons_room(True)
+                case 1:
+                    print(
+                        "You've never needed a weapon before, so why should you need one now? You leave the dagger on the shelf.")
+                    discoveredOptions['dagger'] = False
+                    SmugglersHideout.describe_weapons_room(True)
+                case 2:
+                    SmugglersHideout.describe_weapons_room(True)
+        else:
+            print("A dozen or so weapons hang on a wooden rack that seems to have been jerry rigged from scraps of rotting wood. \n"
+              "Most you don't know how to use, and they are all in disrepair, with spots of rust on some, and on others it \n"
+              "is difficult to tell the rust from the weapon itself. While you don't know how to use a bladed weapon, one \n"
+              "implement in particular catches your eye. A small club in the bottom right of the could offer you something \n"
+              "in a fight. There's also a dagger hanging off of a particularly precarious hook that has less rust than the \n"
+              "rest, and has an edge.") #TODO: add option to choose if have both.
+            decision = decide(['take club','take dagger', 'go back'], False)
+            match decision:
+                case 0:
+                    print("The club falls heavily into your hand, and offers a reassuring presence, though you hope you won't need it.")
+                    discoveredOptions['club'] = True
+                    SmugglersHideout.describe_weapons_room(True)
+                case 1:
+                    print("The dagger feels heavy in your hand, though you aren't sure, if push came to shove, you would be able to use it.")
+                    discoveredOptions['dagger'] = True
+                    SmugglersHideout.describe_weapons_room(True)
+                case 2:
+                    SmugglersHideout.describe_weapons_room(True)
+    def inspect_tools_in_storage():
+        print(f"\nDEBUG: {discoveredOptions}\n")
+        if discoveredOptions['lock picks']:
+            print(
+                'While the lock picks will work  on a simple lock, you could also take the key imprint device, or drop the \n'
+                'lock picks here. If you don\'t need them, they will just get in the way.')
+            decision = decide(['take key wax', 'leave lock picks', 'go back'], False)
+            match decision:
+                case 0:
+                    if discoveredOptions['key wax']:
+                        print("You already have this")
+                        SmugglersHideout.inspect_tools_in_storage()
+                    else:
+                        print(
+                        "You slip the key wax into your pocket, it's better to be safe than sorry.")
+                        discoveredOptions['key wax'] = True
+                        SmugglersHideout.describe_weapons_room(True)
+                case 1:
+                    print("You may as well drop it off here if you don't need it so it doesn't get in the way")
+                    discoveredOptions['lock picks'] = False
+                    SmugglersHideout.describe_weapons_room(True)
+                case 2:
+                    SmugglersHideout.describe_weapons_room(True)
+        elif discoveredOptions['key wax']:
+            print(
+                "The key wax is pretty slow, it might be useful to take the lock picks. Or you could just drop it off here \n"
+                "so as not to get in the way"
+            )
+            decision = decide(['take lock picks', 'leave key wax', 'go back'], False)
+            match decision:
+                case 0:
+                    if discoveredOptions['lock picks']:
+                        print("You already have this")
+                        SmugglersHideout.inspect_tools_in_storage()
+                    else:
+                        print(
+                            "You slip the lock picks into your pocket, it's better to be safe than sorry.")
+                        discoveredOptions['lock picks'] = True
+                        SmugglersHideout.describe_weapons_room(True)
+                case 1:
+                    print("You may as well drop it off here if you don't need it so it doesn't get in the way")
+                    discoveredOptions['key wax'] = False
+                    SmugglersHideout.describe_weapons_room(True)
+                case 2:
+                    SmugglersHideout.describe_weapons_room(True)
+        else:
+            print(
+                "A couple tools sit on the shelf, though you haven't bothered to learn most of them. \n"
+                "The two you are confident you can use are the lock picking set and the key imprint device. \n"
+                "The lock picking set is quicker, but the key imprint device (key wax) will work on any key. \n"
+                "Both are small enough to fit in your pocket")
+            decision = decide(['take lock picking set', 'take key imprint device', 'go back'], False)
+            match decision:
+                case 0:
+                    print(
+                        "The club falls heavily into your hand, and offers a reassuring presence, though you hope you won't need it.")
+                    discoveredOptions['club'] = True
+                    SmugglersHideout.describe_weapons_room(True)
+                case 1:
+                    print(
+                        "The dagger feels heavy in your hand, though you aren't sure, if push came to shove, you would be able to use it.")
+                    discoveredOptions['dagger'] = True
+                    SmugglersHideout.describe_weapons_room(True)
+                case 2:
+                    SmugglersHideout.describe_weapons_room(True)
     def describe_storage_room():
-        print("You are going where noone has gone before. This path has not been implimented yet.")
+        print("You are going where noone has gone before. This path has not been implemented yet.")
         SmugglersHideout.describe_back_hall(True)
     def describe_bunk_room():
-        print("You are going where noone has gone before. This path has not been implimented yet.")
+        print("You are going where noone has gone before. This path has not been implemented yet.")
         SmugglersHideout.describe_back_hall(True)
-class Graysouls:
-    def describe_outside_safehouse():
-        print("You are going where noone has gone before. This path has not been implimented yet.")
-        SmugglersHideout.describe_main_room(True)
+
 if __name__ == "__main__":
     SmugglersHideout.describe_main_room(None)
